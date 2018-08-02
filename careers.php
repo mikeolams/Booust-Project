c53f569d8cec2a429cf0cc923f1c916a30862b2b
Michael.Olamikan <mikeolams@gmail.com>
2018-08-02 12:59:56 +0100

    test changes for week9

--- careers.php
+++ careers.php
@@ -14,7 +14,7 @@
 <body>
 <nav aria-label="breadcrumb">
   <ol class="breadcrumb">
-    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+    <li class="breadcrumb-item"><a href="home.php">Home</a></li>
     <li class="breadcrumb-item active" aria-current="page">Careers</li>
   </ol>
 </nav>
@@ -24,7 +24,7 @@
   <p class="lead">Currently, there are no vacancies but please keep in touch for we shall be recruiting very soon.</p>
   <hr class="my-4">
   <p>Therefore If you like to call our attention to your outstanding skills and CV. Hit the button below.</p>
-  <a class="btn btn-primary btn-lg" href="index.html" role="button">Send us your CV</a>
+  <a class="btn btn-primary btn-lg" href="home.php" role="button">Send us your CV</a>
 </div>
 </div>
 <script type="text/javascript" src = js/jquery.js></script>
